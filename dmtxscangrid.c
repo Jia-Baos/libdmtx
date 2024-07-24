70fd826598a839730c8732664954ae042dff4e43
Jia-Baos <18383827268@163.com>
2024-07-24 22:03:21 +0800

    2024-07-24 22:03

--- dmtxscangrid.c
+++ dmtxscangrid.c
@@ -32,10 +32,10 @@ InitScanGrid(DmtxDecode *dec)
    scale = dmtxDecodeGetProp(dec, DmtxPropScale);
    smallestFeature = dmtxDecodeGetProp(dec, DmtxPropScanGap) / scale;
 
-   grid.xMin = dmtxDecodeGetProp(dec, DmtxPropXmin);
-   grid.xMax = dmtxDecodeGetProp(dec, DmtxPropXmax);
-   grid.yMin = dmtxDecodeGetProp(dec, DmtxPropYmin);
-   grid.yMax = dmtxDecodeGetProp(dec, DmtxPropYmax);
+   grid.xMin = dmtxDecodeGetProp(dec, DmtxPropXmin);  // 0
+   grid.xMax = dmtxDecodeGetProp(dec, DmtxPropXmax);  // width of img -1
+   grid.yMin = dmtxDecodeGetProp(dec, DmtxPropYmin);  // 0
+   grid.yMax = dmtxDecodeGetProp(dec, DmtxPropYmax);  // height of img -1
 
    /* Values that get set once */
    xExtent = grid.xMax - grid.xMin;
@@ -45,18 +45,18 @@ InitScanGrid(DmtxDecode *dec)
    assert(maxExtent > 1);
 
    for(extent = 1; extent < maxExtent; extent = ((extent + 1) * 2) - 1)
-      if(extent <= smallestFeature)
+      if(extent <= smallestFeature) // 根据 smallestFeature 确定 minExtent
          grid.minExtent = extent;
 
-   grid.maxExtent = extent;
+   grid.maxExtent = extent;   // maxExtent大于img的宽高，等于2^n - 1
 
-   // 图像在grid中央，左下角为原点，此处的偏移量为网格中点在图像坐标系中的坐标
+   // img在grid中央，img的左下角为原点，此处的偏移量为grid中左下角点在img坐标系中的坐标
    grid.xOffset = (grid.xMin + grid.xMax - grid.maxExtent) / 2;
    grid.yOffset = (grid.yMin + grid.yMax - grid.maxExtent) / 2;
 
    /* Values that get reset for every level */
-   grid.total = 1;
-   grid.extent = grid.maxExtent;
+   grid.total = 1;   // ？
+   grid.extent = grid.maxExtent; // ？
 
    SetDerivedFields(&grid);
 
@@ -104,18 +104,18 @@ GetGridCoordinates(DmtxScanGrid *grid, DmtxPixelLoc *locPtr)
    /* Jump to next cross pattern horizontally if current column is done */
    if(grid->pixelCount >= grid->pixelTotal) {
       grid->pixelCount = 0;
-      grid->xCenter += grid->jumpSize;
+      grid->xCenter += grid->jumpSize; // if current cloumn is done, xCenter is ?
    }
 
    /* Jump to next cross pattern vertically if current row is done */
    if(grid->xCenter > grid->maxExtent) {
       grid->xCenter = grid->startPos;
-      grid->yCenter += grid->jumpSize;
+      grid->yCenter += grid->jumpSize; // if current row is done, yCenter is ?
    }
 
    /* Increment level when vertical step goes too far */
    if(grid->yCenter > grid->maxExtent) {
-      grid->total *= 4;
+      grid->total *= 4; // 遍历更小的区域，十字的左上、左下、右上、右下
       grid->extent /= 2;
       SetDerivedFields(grid);
    }
@@ -140,18 +140,18 @@ GetGridCoordinates(DmtxScanGrid *grid, DmtxPixelLoc *locPtr)
 
       /* horizontal portion */
       if(count < half) {
-         loc.X = grid->xCenter + ((count < quarter) ? (count - quarter) : (half - count));
+         loc.X = grid->xCenter + ((count < quarter) ? (count - quarter) : (half - count));   // 由左到右遍历
          loc.Y = grid->yCenter;
       }
       /* vertical portion */
       else {
          count -= half;
          loc.X = grid->xCenter;
-         loc.Y = grid->yCenter + ((count < quarter) ? (count - quarter) : (half - count));
+         loc.Y = grid->yCenter + ((count < quarter) ? (count - quarter) : (half - count));   // 由下到上遍历
       }
    }
 
-   loc.X += grid->xOffset;
+   loc.X += grid->xOffset; // 校正到img坐标系
    loc.Y += grid->yOffset;
 
    *locPtr = loc;
@@ -161,7 +161,7 @@ GetGridCoordinates(DmtxScanGrid *grid, DmtxPixelLoc *locPtr)
 
    if(loc.X < grid->xMin || loc.X > grid->xMax ||
          loc.Y < grid->yMin || loc.Y > grid->yMax)
-      return DmtxRangeBad;
+      return DmtxRangeBad; // 无效坐标（不在img区域中）
 
    return DmtxRangeGood;
 }
@@ -176,7 +176,7 @@ SetDerivedFields(DmtxScanGrid *grid)
 {
    grid->jumpSize = grid->extent + 1;  // 遍历完行后去遍历列
    grid->pixelTotal = 2 * grid->extent - 1;  // 十字上所有的像素点
-   grid->startPos = grid->extent / 2;  // 遍历起始位置
+   grid->startPos = grid->extent / 2;  // 十字结构中心的坐标
    grid->pixelCount = 0;
-   grid->xCenter = grid->yCenter = grid->startPos; // 十字的中心，即遍历的起始位置
+   grid->xCenter = grid->yCenter = grid->startPos; // 十字结构的中心，遍历时从十字的顶点开始
 }
